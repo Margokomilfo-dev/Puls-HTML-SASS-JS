74b03131750fa0fa3f6baa6f757b601bf4cdbc67
Margo <margokomilfo@mail.ru>
2020-04-19 21:59:24 +0200

    Puls -mod - 100%

---
+++ js/script.js
@@ -0,0 +1,133 @@
+/* $(document).ready(function(){
+$('.carousel__inner').slick({
+  speed: 1200,
+  adaptiveHeight: true,
+  prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow_left.png"></button>',
+  nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow_right.png"></button>',
+  responsive: [
+    {
+    breakpoint: 1024,
+    settings: {
+      arrows: false,
+      dots: true
+      }
+    }
+  ]
+});
+}); */
+
+$(document).ready(function(){
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+  function toggleSlide(item) {
+    $(item).each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+    });
+  };
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+
+  const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    controls: false,
+    nav: false
+  });
+
+  document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+  });
+
+  document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+  });
+
+  //modal
+  $('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow');
+  });
+
+  $('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+  });
+
+  $('.button_mini').each(function(i){
+    $(this).on('click', function(){
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+    });
+  });
+
+  function validateForm(form){
+    $(form).validate({
+      rules: {
+        name: 'required',
+        phone: 'required',
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста введите свое имя"
+        },
+        phone: {
+          required: "Пожалуйста введите свой номер телефона"
+        },
+        email: {
+          required: "Пожалуйста введите свою почту",
+          email: "Неправильно введен email"
+        }
+      }
+    });
+  }
+
+  validateForm('#consultation-form');
+  validateForm('#order form');
+  validateForm('#consultation form');
+
+  $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+  $('form').submit(function(e){
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function(){
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn();
+      $('form').trigger('reset');
+    });
+    return false;
+  });
+
+  //smooth scroll and pageup
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 1400) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+  //scroll
+  $("a[href^='#up']").click(function(){ 
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
+  //animated
+  new WOW().init();
+});
